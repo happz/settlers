69fa4c233fdeef77dccd13973fefcb1daa12ac3b
Milos Prchlik <happz@happz.cz>
2012-11-29 22:16:57 +0100

    Simple snapshot of statistics, for some future graphs

--- src/handlers/monitor.py
+++ src/handlers/monitor.py
@@ -1,5 +1,6 @@
 import handlers
 
+import hlib.api
 import hlib.stats
 
 from handlers import page, require_login
@@ -9,3 +10,7 @@ class Handler(handlers.GenericHandler):
   @page
   def index(self):
     return self.generate('monitor.mako', params = {'stats': hlib.stats.snapshot(hlib.stats.stats)})
+
+  @hlib.api.api
+  def snapshot(self):
+    return hlib.api.Reply(200, snapshot = hlib.stats.snapshot(hlib.stats.stats))
