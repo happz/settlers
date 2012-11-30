6e48e8a71b3909f55545ca13e972a0e8c9cc4f64
Milos Prchlik <happz@happz.cz>
2012-11-30 20:53:29 +0100

    Just a small spelling check

--- src/handlers/monitor.py
+++ src/handlers/monitor.py
@@ -3,7 +3,10 @@ import handlers
 import hlib.api
 import hlib.stats
 
+import hruntime
+
 from handlers import page, require_login
+from hlib.api import api
 
 class Handler(handlers.GenericHandler):
   @require_login
@@ -11,6 +14,6 @@ class Handler(handlers.GenericHandler):
   def index(self):
     return self.generate('monitor.mako', params = {'stats': hlib.stats.snapshot(hlib.stats.stats)})
 
-  @hlib.api.api
-  def snapshot(self):
+  @api
+  def snapshot_stats(self):
     return hlib.api.Reply(200, snapshot = hlib.stats.snapshot(hlib.stats.stats))
